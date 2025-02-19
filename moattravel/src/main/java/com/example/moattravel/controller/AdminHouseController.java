1992149e4770efcae0b74af59922136173f1207a
oyama <oyama@192.168.10.103>
2025-02-19 17:57:15 +0900

    2/19 18:00のプッシュ

--- moattravel/src/main/java/com/example/moattravel/controller/AdminHouseController.java
+++ moattravel/src/main/java/com/example/moattravel/controller/AdminHouseController.java
@@ -1,7 +1,9 @@
 package com.example.moattravel.controller;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,8 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.moattravel.entity.House;
 import com.example.moattravel.repository.HouseRepository;
-
-
 
 @Controller
 @RequestMapping("/admin/houses")
@@ -23,12 +23,18 @@ public class AdminHouseController {
 	}
 	
 	@GetMapping
-	public String index(Model model) {
-		List<House> houses = houseRepository.findAll();
+//	public String index(Model model) {
+//	public String index(Model model, Pageable pageable) {
+		public String index(Model model, @PageableDefault(page =0,size=10, sort="id", direction= Direction.ASC) Pageable pageable) {
 		
-		model.addAttribute("houses" , houses);
+//		List<House> houses = houseRepository.findAll();
+		Page<House> housePage = houseRepository.findAll(pageable);
+	
+//		model.addAttribute("houses" , houses);
+		model.addAttribute("housePage", housePage);
 		
 		return "admin/houses/index";
 	}
+	
 
 }
