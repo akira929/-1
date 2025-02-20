dadaacff90929a687b879688f0a09ec74c3a7fa8
oyama <oyama@192.168.10.103>
2025-02-20 13:00:57 +0900

    2/20 13:00のプッシュ

--- moattravel/src/main/java/com/example/moattravel/controller/AdminHouseController.java
+++ moattravel/src/main/java/com/example/moattravel/controller/AdminHouseController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.moattravel.entity.House;
 import com.example.moattravel.repository.HouseRepository;
@@ -25,13 +26,24 @@ public class AdminHouseController {
 	@GetMapping
 //	public String index(Model model) {
 //	public String index(Model model, Pageable pageable) {
-		public String index(Model model, @PageableDefault(page =0,size=10, sort="id", direction= Direction.ASC) Pageable pageable) {
+//		public String index(Model model, @PageableDefault(page =0,size=10, sort="id", direction= Direction.ASC) Pageable pageable) {
+		public String index(Model model, @PageableDefault(page =0, size =10, sort ="id", direction=Direction.ASC) Pageable pageable, 
+				@RequestParam(name = "keyword", required = false) String keyword) {
 		
 //		List<House> houses = houseRepository.findAll();
-		Page<House> housePage = houseRepository.findAll(pageable);
+//		Page<House> housePage = houseRepository.findAll(pageable);
+		Page<House>housePage;
+		
+		if (keyword != null && !keyword.isEmpty()) {
+			housePage =houseRepository.findByNameLike("%" + keyword + "%", pageable);
+		} else {
+			housePage = houseRepository.findAll(pageable);
+		}
 	
 //		model.addAttribute("houses" , houses);
 		model.addAttribute("housePage", housePage);
+		model.addAttribute("keyword", keyword);
+		
 		
 		return "admin/houses/index";
 	}
